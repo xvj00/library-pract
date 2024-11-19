6fa96403f0045c2589e7f3c851f940900a4389a1
xvj <emsjrhux@gmail.com>
2024-11-19 12:27:12 +0300

    эщкере

--- resources/views/components/nav-menu.blade.php
+++ resources/views/components/nav-menu.blade.php
@@ -1,11 +1,19 @@
 <div class="flex w-full items-center p-4 bg-white rounded-lg">
     <!-- Изображение профиля -->
-    @auth()
-    <img src="{{ auth()->user()->getFirstMediaUrl('user_images') }}" class=" object-cover w-16 h-16 rounded-full border-2 border-gray-300 " alt="Профиль">
-    @else
-        <img src="{{asset('img/man.svg') }}" class="w-16 h-16 rounded-full border-2 border-gray-300 " alt="Профиль">
+    @auth
+        @if(auth() -> user() ->getFirstMediaUrl('user_images'))
+            <img src="{{ auth()->user()->getFirstMediaUrl('user_images') }}"
+                 class="object-cover w-16 h-16 rounded-full border-2 border-gray-300" alt="Профиль">
+        @else
+            <img src="{{ asset('img/man.svg') }}" class="w-16 h-16 rounded-full border-2 border-gray-300" alt="Профиль">
 
+        @endif
     @endauth
+
+    @guest
+        <img src="{{ asset('img/man.svg') }}" class="w-16 h-16 rounded-full border-2 border-gray-300" alt="Профиль">
+    @endguest
+
     <div class="flex-1 ml-4 flex flex-col items-center text-gray-800">
         <p class="text-lg font-semibold tracking-wider text-gray-700">Профиль</p>
 
@@ -16,10 +24,12 @@
         @else
             <!-- Кнопки Вход и Регистрация -->
             <div class="flex space-x-4 mt-2">
-                <a href="{{ route('login') }}" class="px-4 py-2 bg-green-500 text-white rounded-md shadow-md hover:bg-green-600 transition">
+                <a href="{{ route('login') }}"
+                   class="px-4 py-2 bg-green-500 text-white rounded-md shadow-md hover:bg-green-600 transition">
                     Вход
                 </a>
-                <a href="{{ route('register') }}" class="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 transition">
+                <a href="{{ route('register') }}"
+                   class="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 transition">
                     Регистрация
                 </a>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,7 @@ use \App\Http\Controllers\LibrarianController;
 Route::get('/', [BookController::class, 'index'])->name('book.index');
 Route::get('/catalog', [BookController::class, 'showCatalog'])->name('book.catalog');
 
-//Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-//Route::delete('/admin/{user}', [AdminController::class, 'destroy'])->name('admin.destroy');
 
-//Route::get('/books', [BookController::class, 'index']);
-//Route::get('/books', [BookController::class, 'create']);
-
-//Route::resource('book', \App\Http\Controllers\BookController::class);
 
 Route::get('/profile', function () {
     return view('pages.user.user_dashboard');
@@ -27,7 +21,7 @@ Route::get('/profile', function () {
 
 Route::middleware('auth')->group(function () {
 
-//    Route::get('/book', [BookController::class, 'index'])->name('book.index');
+
     Route::post('reservations', [ReservationController::class, 'store'])->name('reservations.store');
     Route::get('profile/reservations', [ReservationController::class, 'userReservations'])->name('reservation.user.index');
     Route::post('profile/reservations/{book}/cancel', [ReservationController::class, 'cancel'])->name('reservation.user.cancel');
@@ -41,9 +35,9 @@ Route::middleware('auth')->group(function () {
         Route::resource('author', AuthorController::class);
         Route::resource('genre', GenresController::class);
         Route::resource('edition', EditionController::class);
-        Route::resource('book', BookController::class);
+//        Route::resource('book', BookController::class);
         Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
-        Route::post('/book', [BookController::class, 'store'])->name('book.store');
+        Route::post('/book/store', [BookController::class, 'store'])->name('book.store');
         Route::get('/book/{book}/edit', [BookController::class, 'edit'])->name('book.edit');
         Route::patch('/book/{book}/update', [BookController::class, 'update'])->name('book.update');
         Route::delete('/book/{book}/destroy', [BookController::class, 'destroy'])->name('book.destroy');
